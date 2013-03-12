0f6a99617f29a437b666fec4c24a5601c577f32f
Richard Klancer <rpk@pobox.com>
2013-03-11 22:03:02 -0400

    Grab touchjoy's joystick/differential drive code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+// Interpreted from what we did for the Artisans Asylum bot
+function interpretMotorCommand(x, y, callback) {
+  var angle = Math.atan2(y, x) - Math.PI/4,
+      speed = Math.sqrt(x*x + y*y),
+      left  = Math.sin(angle),
+      right = Math.cos(angle),
+      scale = speed / Math.max(Math.abs(left), Math.abs(right));
+
+  if (speed > 1.0) {
+    throw new Error("Joystick input outside the unit circle");
+  }
+  callback(scale * left, scale * right);
+}
+
+function move(left, right) {
+  console.log(left, right);
+}
+
 touchjoy(100, function(x, y) {
-  console.log("x: " + x + ", y: " + y);
+  interpretMotorCommand(x, y, move);
 });
